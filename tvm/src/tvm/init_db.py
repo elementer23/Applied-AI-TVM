2f4b916ed7fb8706826000176b4dd12529d18521
Road-Train <trol13xd@gmail.com>
2025-05-28 14:08:26 +0200

    You can now load the data into the db.

---
+++ tvm/src/tvm/init_db.py
@@ -0,0 +1,42 @@
+import json
+from tvm.models import Category, SubCategory, AdvisoryText
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+# uses knowledge/templates.json to seed the database with data in absence of a front-end.
+
+engine = create_engine("sqlite:///./db.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with open("knowledge/templates.json") as f:
+    data = json.load(f)
+
+# Create categories
+name_to_cat = {}
+for c in data["categories"]:
+    cat = Category(name=c["name"])
+    session.add(cat)
+    session.flush()  # get ID without full commit
+    name_to_cat[c["name"]] = cat.id
+
+
+# Create subcategories
+name_to_sub = {}
+for s in data["subcategories"]:
+    sub = SubCategory(name=s["name"])
+    session.add(sub)
+    session.flush()
+    name_to_sub[s["name"]] = sub.id
+
+# Create advisory texts
+for a in data["advisory_texts"]:
+    advisory = AdvisoryText(
+        category=name_to_cat[a["category"]],
+        sub_category=name_to_sub[a["sub_category"]],
+        text=a["text"]
+    )
+    session.add(advisory)
+
+session.commit()
+session.close()
