38e344b741b7f55cf01acbe70cd80698da0c8c67
jariolyslager <jari.olijslager@student.nhlstenden.com>
2025-05-29 06:42:48 +0200

    Added code and unit tests to only allow admins to access certain endpoints

--- tvm/src/tvm/text_endpoints.py
+++ tvm/src/tvm/text_endpoints.py
@@ -1,4 +1,5 @@
 from main import app, get_db
+from authentication import *
 from fastapi import Depends, HTTPException, status, Response
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -31,46 +32,55 @@ def read_category(category_id: int, db: Session = Depends(get_db)):
 
 # Create a new category
 @app.post("/categories/")
-def create_category(category_name: str, db: Session = Depends(get_db)):
+def create_category(category_name: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Create a new category of risk.
     """
-    category = db.get(Category, category_name)
-    if category:
-        raise HTTPException(status_code=400, detail="This category already exists.")
-    new_category = Category(text=category_name)
-    db.add(new_category)
-    db.commit()
-    db.refresh(new_category)
-    return Response(status_code=201, content=f"Category {new_category.text} successfully created.")
+    if current_user.role == "admin":
+        category = db.get(Category, category_name)
+        if category:
+            raise HTTPException(status_code=400, detail="This category already exists.")
+        new_category = Category(text=category_name)
+        db.add(new_category)
+        db.commit()
+        db.refresh(new_category)
+        return Response(status_code=201, content=f"Category {new_category.text} successfully created.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to create a category.")
 
 # Update the category with the given ID
 @app.put("/categories/{category_id}")
-def update_category(category_id: int, category_name: str, db: Session = Depends(get_db)):
+def update_category(category_id: int, category_name: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Update a specific category of risk for the given ID.
     """
-    category = db.get(Category, category_id)
-    if not category:
-        return HTTPException(status_code=404, detail="Category not found.")
-    setattr(category, Category.text, category_name)
-    db.commit()
-    db.refresh(category)
-    return Response(status_code=200, content=f"Category {category_name} successfully updated.")
+    if current_user.role == "admin":
+        category = db.get(Category, category_id)
+        if not category:
+            return HTTPException(status_code=404, detail="Category not found.")
+        setattr(category, Category.text, category_name)
+        db.commit()
+        db.refresh(category)
+        return Response(status_code=200, content=f"Category {category_name} successfully updated.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to update a category.")
 
 # Delete the category with the given ID
 @app.delete("/categories/{category_id}")
-def delete_category(category_id: int, db: Session = Depends(get_db)):
+def delete_category(category_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Delete the category of risk corresponding to the given ID.
     """
-    category = db.get(Category, category_id)
-    if not category:
-        return HTTPException(status_code=404, detail="Category not found.")
-    db.delete(category)
-    db.commit()
-    db.refresh(category)
-    return Response(status_code=204, content=f"Category {category} successfully deleted.")
+    if current_user.role == "admin":
+        category = db.get(Category, category_id)
+        if not category:
+            return HTTPException(status_code=404, detail="Category not found.")
+        db.delete(category)
+        db.commit()
+        db.refresh(category)
+        return Response(status_code=204, content=f"Category {category} successfully deleted.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to delete a category.")
 
 # Get all subcategories
 @app.get("/subcategories/")
@@ -96,46 +106,55 @@ def read_subcategory(subcategory_id: int, db: Session = Depends(get_db)):
 
 # Create a new subcategory
 @app.post("/subcategories/")
-def create_subcategory(subcategory_name: str, db: Session = Depends(get_db)):
+def create_subcategory(subcategory_name: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Creates a new subcategory of risk willingness.
     """
-    subcategory = db.query(SubCategory).filter(SubCategory.name == subcategory_name).first()
-    if subcategory:
-        raise HTTPException(status_code=400, detail="This subcategory already exists.")
-    new_subcategory = SubCategory(text=subcategory_name)
-    db.add(new_subcategory)
-    db.commit()
-    db.refresh(new_subcategory)
-    return Response(status_code=201, content=f"Subcategory {subcategory_name} successfully created.")
+    if current_user.role == "admin":
+        subcategory = db.query(SubCategory).filter(SubCategory.name == subcategory_name).first()
+        if subcategory:
+            raise HTTPException(status_code=400, detail="This subcategory already exists.")
+        new_subcategory = SubCategory(text=subcategory_name)
+        db.add(new_subcategory)
+        db.commit()
+        db.refresh(new_subcategory)
+        return Response(status_code=201, content=f"Subcategory {subcategory_name} successfully created.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to create a subcategory.")
 
 # Update the subcategory with the given ID
 @app.put("/subcategories/{subcategory_id}")
-def update_subcategory(subcategory_id: int, subcategory_name: str, db: Session = Depends(get_db)):
+def update_subcategory(subcategory_id: int, subcategory_name: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Update a specific subcategory of risk willingness for the given ID.
     """
-    subcategory = db.get(SubCategory, subcategory_id)
-    if not subcategory:
-        return HTTPException(status_code=404, detail="Subcategory not found.")
-    setattr(subcategory, SubCategory.text, subcategory_name)
-    db.commit()
-    db.refresh(subcategory)
-    return Response(status_code=200, content=f"Subcategory {subcategory_name} successfully updated.")
+    if current_user.role == "admin":
+        subcategory = db.get(SubCategory, subcategory_id)
+        if not subcategory:
+            return HTTPException(status_code=404, detail="Subcategory not found.")
+        setattr(subcategory, SubCategory.text, subcategory_name)
+        db.commit()
+        db.refresh(subcategory)
+        return Response(status_code=200, content=f"Subcategory {subcategory_name} successfully updated.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to update a subcategory.")
 
 # Delete the subcategory with the given ID
 @app.delete("/subcategories/{subcategory_id}")
-def delete_category(subcategory_id: int, db: Session = Depends(get_db)):
+def delete_category(subcategory_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Delete the subcategory of risk willingness corresponding to the given ID.
     """
-    subcategory = db.get(SubCategory, subcategory_id)
-    if not subcategory:
-        return HTTPException(status_code=404, detail="Subcategory not found.")
-    db.delete(subcategory)
-    db.commit()
-    db.refresh(subcategory)
-    return Response(status_code=204, content=f"Subcategory {subcategory} successfully deleted.")
+    if current_user.role == "admin":
+        subcategory = db.get(SubCategory, subcategory_id)
+        if not subcategory:
+            return HTTPException(status_code=404, detail="Subcategory not found.")
+        db.delete(subcategory)
+        db.commit()
+        db.refresh(subcategory)
+        return Response(status_code=204, content=f"Subcategory {subcategory} successfully deleted.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to delete a subcategory.")
 
 # Gets all advice texts
 @app.get("/advisorytexts/")
@@ -161,43 +180,52 @@ def read_text(catid: int, subid: int, db: Session = Depends(get_db)):
 
 # Create new advice text
 @app.post("/advisorytexts/")
-def create_text(advice_text: str, db: Session = Depends(get_db)):
+def create_text(advice_text: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Creates a new advisory text template.
     """
-    advice = db.query(AdvisoryText).filter(AdvisoryText.text == advice_text).first()
-    if advice:
-        raise HTTPException(status_code=400, detail="This text already exists.")
-    new_advice = AdvisoryText(text=advice_text)
-    db.add(new_advice)
-    db.commit()
-    db.refresh(new_advice)
-    return Response(status_code=201, content=f"AdvisoryText {new_advice.text} successfully created.")
+    if current_user.role == "admin":
+        advice = db.query(AdvisoryText).filter(AdvisoryText.text == advice_text).first()
+        if advice:
+            raise HTTPException(status_code=400, detail="This text already exists.")
+        new_advice = AdvisoryText(text=advice_text)
+        db.add(new_advice)
+        db.commit()
+        db.refresh(new_advice)
+        return Response(status_code=201, content=f"AdvisoryText {new_advice.text} successfully created.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to create an advisory text.")
 
 # Edits the advice text with the given ID
 @app.put("/advisorytexts/catid={category_id}&subid={subcategory_id}")
-def update_text(catid: int, subid: int, advice_text: str, db: Session = Depends(get_db)):
+def update_text(catid: int, subid: int, advice_text: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Updates an existing advisory text template for a given category and subcategory.
     """
-    advisorytext = db.query(AdvisoryText).filter(AdvisoryText.category_id == catid && AdvisoryText.subcategory_id == subid).first()
-    if not advisorytext:
-        return HTTPException(status_code=404, detail="Advice text not found.")
-    setattr(advisorytext, AdvisoryText.text, advice_text)
-    db.commit()
-    db.refresh(advisorytext)
-    return Response(status_code=200, content=f"Advisory Text {advice_text} successfully updated.")
+    if current_user.role == "admin":
+        advisorytext = db.query(AdvisoryText).filter(AdvisoryText.category_id == catid && AdvisoryText.subcategory_id == subid).first()
+        if not advisorytext:
+            return HTTPException(status_code=404, detail="Advice text not found.")
+        setattr(advisorytext, AdvisoryText.text, advice_text)
+        db.commit()
+        db.refresh(advisorytext)
+        return Response(status_code=200, content=f"Advisory Text {advice_text} successfully updated.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to update an advisory text.")
 
 # Deletes the advice text with the given ID
 @app.delete("/advisorytexts/catid={category_id}&subid={subcategory_id}")
-def delete_text(catid: int, subid: int, db: Session = Depends(get_db)):
+def delete_text(catid: int, subid: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
     Deletes a specific advisory text template for a given category and subcategory.
     """
-    advisorytext = db.query(AdvisoryText).filter(AdvisoryText.category_id == catid && AdvisoryText.subcategory_id == subid).first()
-    if not advisorytext:
-        return HTTPException(status_code=404, detail="Advice text not found.")
-    db.delete(advisorytext)
-    db.commit()
-    db.refresh(advisorytext)
-    return Response(status_code=204, content=f"Subcategory {advisorytext} successfully deleted.")
+    if current_user.role == "admin":
+        advisorytext = db.query(AdvisoryText).filter(AdvisoryText.category_id == catid && AdvisoryText.subcategory_id == subid).first()
+        if not advisorytext:
+            return HTTPException(status_code=404, detail="Advice text not found.")
+        db.delete(advisorytext)
+        db.commit()
+        db.refresh(advisorytext)
+        return Response(status_code=204, content=f"Subcategory {advisorytext} successfully deleted.")
+    else:
+        raise HTTPException(status_code=403, detail="You are not allowed to delete an advisory text.")
--- tvm/test/test_text_endpoints.py
+++ tvm/test/test_text_endpoints.py
@@ -2,6 +2,7 @@ from tvm.text_endpoints import *
 
 from test_main import client
 
+### Some form of authorization needs to be added to all tests where the user is supposed to be an admin.
 
 # Get all categories
 def test_read_categories():
@@ -49,6 +50,17 @@ def test_create_category_400():
         "detail": "This category already exists."
     }
 
+def test_create_category_403():
+    category_data = {
+        # Enter data of a category that already exists within the db
+    }
+
+    response = client.post("/categories/", json=category_data)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to create a category."
+    }
+
 # Update the category with the given ID
 def test_update_category_200():
     updated_data = {
@@ -69,6 +81,16 @@ def test_update_category_404():
         "detail": "Category not found."
     }
 
+def test_update_category_403():
+    updated_data = {
+        # Enter updated data for a category
+    }
+
+    response = client.put("/categories/3", json=updated_data)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to update a category."
+    }
 
 # Delete the category with the given ID
 def test_delete_category_204():
@@ -82,6 +104,12 @@ def test_delete_category_404():
         "detail": "Category not found."
     }
 
+def test_delete_category_403():
+    response = client.delete("/categories/3")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to delete a category."
+    }
 
 # Get all subcategories
 def test_read_subcategories():
@@ -101,7 +129,6 @@ def test_read_subcategory_200():
     assert response.json() == {
         # Enter data of this subcategory
     }
-
 
 def test_read_subcategory_404():
     response = client.get("/subcategories/999")
@@ -130,6 +157,17 @@ def test_create_subcategory_400():
         "detail": "This subcategory already exists."
     }
 
+def test_create_subcategory_403():
+    subcategory_data = {
+        # Enter data of a subcategory that already exists within the db
+    }
+
+    response = client.post("/subcategories/", json=subcategory_data)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to create a subcategory."
+    }
+
 # Update the subcategory with the given ID
 def test_update_subcategory_200():
     updated_data = {
@@ -138,7 +176,6 @@ def test_update_subcategory_200():
 
     response = client.put("/subcategories/1", json=updated_data)
     assert response.status_code == 200
-
 
 def test_update_subcategory_404():
     updated_data = {
@@ -149,6 +186,17 @@ def test_update_subcategory_404():
     assert response.status_code == 404
     assert response.json() == {
         "detail": "Subcategory not found."
+    }
+
+def test_update_subcategory_403():
+    updated_data = {
+        # Enter updated data for a subcategory
+    }
+
+    response = client.put("/subcategories/3", json=updated_data)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to update a subcategory."
     }
 
 # Delete the subcategory with the given ID
@@ -162,6 +210,13 @@ def test_delete_subcategory_404():
     assert response.status_code == 404
     assert response.json() == {
         "detail": "Subcategory not found."
+    }
+
+def test_delete_subcategory_403():
+    response = client.delete("/subcategories/3")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "You are not allowed to delete a subcategory."
     }
 
 # Gets all advice texts
@@ -182,7 +237,6 @@ def test_read_advisory_text_200():
     assert response.json() == {
         # Enter data of this advisory text
     }
-
 
 def test_read_advisory_text_404():
     response = client.get("/advisorytexts/catid=999&subid=999")
@@ -211,6 +265,17 @@ def test_create_advisory_text_400():
         "detail": "This text already exists."
     }
 
+def test_create_advisory_text_403():
+    advisory_text_data = {
+        # Enter data of an advice text that already exists within the db
+    }
+
+    response = client.post("/advisorytexts/", json=advisory_text_data)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to create an advisory text."
+    }
+
 # Edits the advice text with the given ID
 def test_update_advisory_text_200():
     updated_data = {
@@ -219,7 +284,6 @@ def test_update_advisory_text_200():
 
     response = client.put("/advisorytexts/catid=1&subid=1", json=updated_data)
     assert response.status_code == 200
-
 
 def test_update_advisory_text_404():
     updated_data = {
@@ -232,15 +296,32 @@ def test_update_advisory_text_404():
         "detail": "Advice text not found."
     }
 
+def test_update_advisory_text_403():
+    updated_data = {
+        # Enter updated data for an advisory text
+    }
+
+    response = client.put("/advisorytexts/catid=3&subid=3", json=updated_data)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to update an advisory text."
+    }
+
 # Deletes the advice text with the given ID
 def test_delete_advisory_text_204():
     response = client.delete("/advisorytexts/catid=1&subid=1")
     assert response.status_code == 204
-
 
 def test_delete_advisory_text_404():
     response = client.delete("/advisorytexts/catid=999&subid=999")
     assert response.status_code == 404
     assert response.json() == {
         "detail": "Advice text not found."
+    }
+
+def test_delete_advisory_text_403():
+    response = client.delete("/advisorytexts/catid=3&subid=3")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You are not allowed to delete an advisory text."
     }
