43f570ded42cd4203ac01f494e75fec2b8e40fbd
jariolyslager <jari.olijslager@student.nhlstenden.com>
2025-06-12 06:31:24 +0200

    Additional unit tests for authentication.py

--- tvm/src/tvm/test/test_authentication.py
+++ tvm/src/tvm/test/test_authentication.py
@@ -73,3 +73,59 @@ def test_logout():
     assert response.status_code == 200
 
 # Create user
+def test_create_user_200():
+    token = get_token_admin()
+    response = client.post("/users/?username=newuser&password=newuserpass", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+
+def test_create_user_400():
+    token = get_token_admin()
+    response = client.post("/users/?username=test_user&password=test_pass", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Username already registered"
+    }
+
+# Read user "me"
+def test_read_users_me_admin():
+    token = get_token_admin()
+    response = client.get("/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.json() == {
+        "username": "test_user",
+        "role": "admin"
+    }
+
+def test_read_users_me_not_admin():
+    token = get_token_not_admin()
+    response = client.get("/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.json() == {
+        "username": "test_not_admin",
+        "role": "user"
+    }
+
+def test_read_users_me_401():
+    response = client.get("/me")
+    assert response.status_code == 401
+
+# Verify token
+
+# Get all users
+def test_list_users_200():
+    token = get_token_admin()
+    response = client.get("/users/", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    assert response.json() == [
+        {"id": 1, "username": "test_user", "role": "admin"},
+        {"id": 2, "username": "test_not_admin", "role": "user"}
+    ]
+
+def test_list_users_401():
+    response = client.get("/users/")
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Could not validate credentials"
+    }
+
+# Update user by ID
+
+# Delete user by ID
