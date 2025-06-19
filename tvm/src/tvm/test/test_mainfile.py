a74c46561b6c045b6139869d3fa692a9d20bfdd9
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2025-06-19 10:10:34 +0200

    add test to function name

--- tvm/src/tvm/test/test_mainfile.py
+++ tvm/src/tvm/test/test_mainfile.py
@@ -1,7 +1,7 @@
 from .test_main import *
 
 
-def run_without_access_token():
+def test_run_without_access_token():
     data = {"input": "Test input"}
     response = client.post("/run",json=data)
     assert response.status_code == 401
